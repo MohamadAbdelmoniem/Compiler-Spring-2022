aa5811066234859d58803cd17f83567954f111e4
karim <karimashraf0055@gmail.com>
2022-04-09 21:29:13 +0200

    scanned input string

---
+++ Source/Code/Test.py
@@ -0,0 +1,20 @@
+import nltk as nltk
+import re
+import tokens as tk
+
+input_string = "if 1 then x := 5 ; y := 5 ; end"
+
+#x=re.search("^if\d+then",input_string)
+#print(x)
+tokens = nltk.wordpunct_tokenize(input_string)
+print("tokens are:",tokens)
+for token in tokens:
+    if token in tk.operators.keys():
+        print(token,"is an",tk.operators.get(token))
+
+    if token in tk.symbols.keys():
+        print(token,"is a",tk.symbols.get(token))
+
+    if token in tk.keywords.keys():
+        print(token,"is a keyword",tk.keywords.get(token))
+
