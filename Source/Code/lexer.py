125ca459e1e7304f52ea17aee51ee6551a3b33e3
Ziad Assem <ziad.assem2001@gmail.com>
2022-04-19 00:26:47 +0200

    added the invalid input's outcome

--- Source/Code/lexer.py
+++ Source/Code/lexer.py
@@ -37,14 +37,16 @@ def lexer():
     for token in tokens: #gives the tokens a code symbol
         if token == "if":
             allSymbols = 'i'
-        if token == "then":
+        elif token == "then":
             allSymbols = allSymbols + 't'
-        if token == "else":
+        elif token == "else":
             allSymbols = allSymbols + 'e'
-        if token == 'end':
+        elif token == 'end':
             allSymbols = allSymbols + 'n'
-        if token.isdigit():
+        elif token.isdigit():
             allSymbols = allSymbols + 'z'
+        else:
+            allSymbols = allSymbols + 'x' #not a valid lexeme
     input_code.close()
 
 
@@ -53,22 +55,22 @@ lexer()
 
 
 dfa2 = VisualDFA(
-    states={"q0","q1","q2","q4","q5","q3","q6"},
-    input_symbols={"i","t","e","n","z"},
+    states={"start","if","then","end","stuck","else","integer","identifier", "keyword"},
+    input_symbols={"i","t","e","n","z","y"},
     transitions={
-        "q0":{"i":"q1","t": "q5","e" :"q5","n":"q5","z":"q5"}, #start
-        "q1":{"i":"q5","t": "q5","e" :"q5","n":"q5","z":"q6"},#if
-        "q2":{"i":"q5","t":"q5","e":"q3","n":"q5","z":"q5"},#then
-        "q3":{"i":"q5","t":"q5","e":"q5","n":"q4","z":"q5"},#else
-        "q4":{"i":"q5","t":"q5","e":"q5","n":"q4","z":"q5"},#end
-        "q5":{"i":"q5","t":"q5","e":"q5","n":"q5","z":"q5"},#stuck
-        "q6":{"i":"q5","t":"q2","z":"q6","e":"q5","n":"q5"}#number
+        "start":{"i":"if","t": "stuck","e" :"stuck","n":"stuck","z":"stuck"}, #start
+        "if":{"i":"stuck","t": "stuck","e" :"stuck","n":"stuck","z":"integer"},#if
+        "then":{"i":"stuck","t":"stuck","e":"else","n":"stuck","z":"stuck"},#then
+        "else":{"i":"stuck","t":"stuck","e":"stuck","n":"end","z":"stuck"},#else
+        "end":{"i":"stuck","t":"stuck","e":"stuck","n":"end","z":"stuck"},#end
+        "stuck":{"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"stuck"},#stuck
+        "integer":{"i":"stuck","t":"then","z":"integer","e":"stuck","n":"stuck"}#number condition
+
     },
-    initial_state="q0",
-    final_states={"q4"},
+    initial_state="start",
+    final_states={"end"},
 )
 
 
 
 dfa2.show_diagram(allSymbols,view=True)
-print(allSymbols)
