e51a873ef09dba2a3e4f8207466dd70a8bd2cfa4
karim <karimashraf0055@gmail.com>
2022-04-20 13:30:48 +0200

    tested and updated coding the tokens

--- Source/Code/lexer.py
+++ Source/Code/lexer.py
@@ -6,9 +6,9 @@ from visual_automata.fa.dfa import VisualDFA
 
 
 def lexer():
-    input_code = open("D:\Ziad\Projects\Compiler-Spring-2022\Source\Code\input",#replace by path to input file
+    input_code = open("/Users/karim/Desktop/Compiler-Spring-2022/Source/Code/input",  # replace by path to input file
                       'r')  # open input file in read mode
-    global allSymbols
+    allSymbols=""
     # defining tokens
     operators = "(:=)"
     keywords = "if|then|else|end"
@@ -34,9 +34,9 @@ def lexer():
             print(token, "is a symbol")
         elif re.findall(identifier, token):
             print(token, "is an identifier")
-    for token in tokens: #gives the tokens a code symbol
+    for token in tokens:  # gives the tokens a code symbol
         if token == "if":
-            allSymbols = 'i'
+            allSymbols = allSymbols + 'i'
         elif token == "then":
             allSymbols = allSymbols + 't'
         elif token == "else":
@@ -52,37 +52,51 @@ def lexer():
         elif token.isdigit():
             allSymbols = allSymbols + 'z'
         else:
-            allSymbols = allSymbols + 'x' #not a valid lexeme
+            allSymbols = allSymbols + 'x'  # not a valid lexeme
+
     input_code.close()
+    return allSymbols
 
 
-lexer()
+allSymbols=lexer()
 
 
-
-dfa2 = VisualDFA(
-    states={"start","if","then","end","stuck","else","condition","identifier", "operator","calculate","symbol"},#id=y operator=o symbol=s
-    input_symbols={"i","t","e","n","z","o","d","s"},
+dfa = DFA(
+    states={"start", "if", "then", "end", "stuck", "else", "condition", "identifier", "operator", "calculate",
+            "symbol"},  # id=y operator=o symbol=s
+    input_symbols={"i", "t", "e", "n", "z", "o", "d", "s"},
     transitions={
-        "start":{"i":"if","t": "stuck","e" :"stuck","n":"stuck","z":"stuck","o":"stuck","d":"stuck","s":"stuck"}, #start
-        "if":{"i":"stuck","t": "stuck","e" :"stuck","n":"stuck","z":"condition","o":"stuck","d":"stuck","s":"stuck"},#if
-        "then":{"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"stuck","o":"stuck","d":"identifier","s":"stuck"},#then
-        "else":{"i":"if","t":"stuck","e":"stuck","n":"end","z":"stuck","o":"stuck","d":"identifier","s":"stuck"},#else
-        "end":{"i":"stuck","t":"stuck","e":"stuck","n":"end","z":"stuck","o":"stuck","d":"stuck","s":"stuck"},#end
-        "stuck":{"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"stuck","o":"stuck","d":"stuck","s":"stuck"},#stuck
-        "condition":{"i":"stuck","t":"then","z":"condition","e":"stuck","n":"stuck","o":"stuck","d":"stuck","s":"stuck"},#number condition
-        "identifier" : {"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"stuck","d":"stuck","o":"operator","s":"stuck"},#cant make id = is for now
-        "operator" :  {"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"calculate","d":"calculate","o":"stuck","s":"stuck"},
-        "calculate":{"i":"stuck","t":"stuck","e":"stuck","n":"stuck","z":"calculate","d":"stuck","o":"stuck","s":"symbol"},
-        "symbol" : {"i":"stuck","t":"stuck","e":"else","n":"end","z":"stuck","d":"identifier","o":"stuck","s":"stuck"}
+        "start": {"i": "if", "t": "stuck", "e": "stuck", "n": "stuck", "z": "stuck", "o": "stuck", "d": "stuck",
+                  "s": "stuck"},  # start
+        "if": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "condition", "o": "stuck", "d": "stuck",
+               "s": "stuck"},  # if
+        "then": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "stuck", "o": "stuck", "d": "identifier",
+                 "s": "stuck"},  # then
+        "else": {"i": "if", "t": "stuck", "e": "stuck", "n": "end", "z": "stuck", "o": "stuck", "d": "identifier",
+                 "s": "stuck"},  # else
+        "end": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "end", "z": "stuck", "o": "stuck", "d": "stuck",
+                "s": "stuck"},  # end
+        "stuck": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "stuck", "o": "stuck", "d": "stuck",
+                  "s": "stuck"},  # stuck
+        "condition": {"i": "stuck", "t": "then", "z": "condition", "e": "stuck", "n": "stuck", "o": "stuck",
+                      "d": "stuck", "s": "stuck"},  # number condition
+        "identifier": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "stuck", "d": "stuck",
+                       "o": "operator", "s": "stuck"},  # cant make id = is for now
+        "operator": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "calculate", "d": "calculate",
+                     "o": "stuck", "s": "stuck"},
+        "calculate": {"i": "stuck", "t": "stuck", "e": "stuck", "n": "stuck", "z": "calculate", "d": "stuck",
+                      "o": "stuck", "s": "symbol"},
+        "symbol": {"i": "stuck", "t": "stuck", "e": "else", "n": "end", "z": "stuck", "d": "identifier", "o": "stuck",
+                   "s": "stuck"}
     },
     initial_state="start",
     final_states={"end"},
 )
 
+'''minimizedDFA = DFA.minify(dfa)'''
+dfa= VisualDFA(dfa)
+dfa.show_diagram(allSymbols)
 
 
+'''print(allSymbols)'''
 
-minimizedDFA = VisualDFA.minify(dfa2)
-minimizedDFA.show_diagram(allSymbols,view=True)
-print(allSymbols)
